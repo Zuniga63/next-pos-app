0eba54f950b2c5cfa8d242c3475ed19cf1df5509
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-08-01 09:48:28 -0500

    style: add semi bold weight to Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,12 @@ import { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-body' });
-const poppins = Poppins({ subsets: ['latin'], display: 'swap', weight: ['400', '700'], variable: '--font-heading' });
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '700'],
+  variable: '--font-heading',
+});
 
 export const metadata: Metadata = {
   title: {

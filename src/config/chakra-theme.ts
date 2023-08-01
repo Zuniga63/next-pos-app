9dbda70ff43069229fb3777e7ec5bb1e4fe29397
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-08-01 16:22:51 -0500

    style: move the google font to config folder
    this action is required for use same fonts on tailwind and chakra UI

--- src/config/chakra-theme.ts
+++ src/config/chakra-theme.ts
@@ -1,9 +1,10 @@
+import { inter, poppins } from '@/config/google-fonts';
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   fonts: {
-    heading: 'var(--font-heading)',
-    body: 'var(--font-body)',
+    heading: poppins.style.fontFamily,
+    body: inter.style.fontFamily,
   },
   initialColorMode: 'light',
   useSystemColorMode: false,

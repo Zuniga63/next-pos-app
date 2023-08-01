0b628ee792759d57a294e637abb2831f8f77b39c
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-08-01 18:02:24 -0500

    feat: create service to get and create minor boxes

---
+++ src/services/boxes.service.ts
@@ -0,0 +1,14 @@
+import { IBox } from '@/types';
+import axios from 'axios';
+
+export const boxesApi = axios.create({ baseURL: `${process.env.NEXT_PUBLIC_URL_API}/cashboxes` });
+
+export const getMinorBoxes = async () => {
+  const res = await boxesApi.get<IBox[]>('/minors');
+  return res.data;
+};
+
+export const createMinorBox = async (data: { name: string }) => {
+  const res = await boxesApi.post<IBox>('/minors', data);
+  return res.data;
+};

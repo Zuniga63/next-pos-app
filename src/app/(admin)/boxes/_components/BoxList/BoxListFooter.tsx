64dbe781916eca965096054c1adec075b2bcd840
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-08-10 18:30:37 -0500

    style: change button content and headaer box

--- src/app/(admin)/boxes/_components/BoxList/BoxListFooter.tsx
+++ src/app/(admin)/boxes/_components/BoxList/BoxListFooter.tsx
@@ -7,7 +7,7 @@ export default function BoxListFooter() {
   return (
     <footer className="min-h-[40px] rounded-b-md border-x border-b border-gray-200 bg-gray-50 px-4 py-2 dark:border-dark dark:bg-header">
       <Button colorScheme="purple" width="full" onClick={showGlobalBox}>
-        Ver todas las transacciones
+        Ver caja global
       </Button>
     </footer>
   );
